51ab536bbb71602ac675f62bd6557dd260291126
Nicola Gibson <nicola.k.gibson@gmail.com>
2019-06-27 11:38:06 +0100

    Initial binary search.

--- PartI/game.js
+++ PartI/game.js
@@ -1,18 +1,45 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;
+let targetNumber = randomNumber(1000);
 
-function bubbleSort(randomNumber) {
-  let swapped;
-  do {
-    swapped = false;
-    for (let i = 0; i < a.length - 1; i++) {
-      if (a[i] > a[i + 1]) {
-        let temp = a[i];
-        a[i] = a[i + 1];
-        a[i + 1] = temp;
-        swapped = true;
-      }
-    }
-  } while (swapped);
+function randomNumber(x) {
+  let number = Math.floor(Math.random() * x);
+  return number;
 }
 
-bubbleSort(randomNumber);
+function randomArray(x, y) {
+  let numberArray = [];
+  for (let i = 0; i < x; i++) {
+    numberArray.push(randomNumber(y));
+  }
+  return numberArray;
+}
+//implement linear and binary search - binary search will need the randomArray to be sorted.
+
+function linearSearch(x, n) {
+  for (let i = x; i <= n; i++) {
+    console.log("Guessing" + i);
+    if (i === targetNumber) {
+      return "The random number is " + i + " Well done!";
+    }
+  }
+}
+
+function binarySearch(x, n) {
+  let guess = Math.floor(n - x / 2);
+  let difference = Math.floor(n - x) / 4;
+  for (let i = 0; i <= n / 2; i++) {
+    if (guess === targetNumber) {
+      return guess;
+    } else if (guess < targetNumber) {
+      guess = guess - difference;
+      difference = difference / 2;
+    } else if (guess > targetNumber) {
+      guess = guess + difference;
+      difference = difference / 2;
+    }
+  }
+  return "error";
+}
+
+console.log(binarySearch(1, 1000));
+
+//try recursion call the binary search function with a new minimum and maximum.
