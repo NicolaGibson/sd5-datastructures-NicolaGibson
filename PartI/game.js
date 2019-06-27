429b5e607b663045eceb59a3cf00d46674a3b54a
Nicola Gibson <nicola.k.gibson@gmail.com>
2019-06-27 16:17:33 +0100

    timer function and working linear search

--- PartI/game.js
+++ PartI/game.js
@@ -1,45 +1,72 @@
-let targetNumber = randomNumber(1000);
+function getNanoTime() {
+  let nanotime = process.hrtime();
+  return nanotime[0] * 1000000000 + nanotime[1];
+}
+
+function checkBinaryTime(sizeOfRange, timeValues) {
+  let startTime = getNanoTime();
+  let targetNumber = randomNumber(1000);
+  binarySearch(targetNumber, sizeOfRange);
+  const endTime = getNanoTime();
+  const overallTime = endTime - startTime;
+  return overallTime;
+}
+
+function checkLinearTime(sizeOfRange, timeValues) {
+  let startTime = getNanoTime();
+  let targetNumber = randomNumber(1000);
+  linearSearch(targetNumber, sizeOfRange);
+  const endTime = getNanoTime();
+  const overallTime = endTime - startTime;
+  return overallTime;
+}
 
 function randomNumber(x) {
   let number = Math.floor(Math.random() * x);
   return number;
 }
 
-function randomArray(x, y) {
-  let numberArray = [];
-  for (let i = 0; i < x; i++) {
-    numberArray.push(randomNumber(y));
-  }
-  return numberArray;
-}
-//implement linear and binary search - binary search will need the randomArray to be sorted.
-
-function linearSearch(x, n) {
-  for (let i = x; i <= n; i++) {
-    console.log("Guessing" + i);
+function linearSearch(targetNumber, n) {
+  for (let i = 1; i <= n; i++) {
     if (i === targetNumber) {
-      return "The random number is " + i + " Well done!";
+      return "The random number is " + i + "!";
     }
   }
 }
 
-function binarySearch(x, n) {
-  let guess = Math.floor(n - x / 2);
-  let difference = Math.floor(n - x) / 4;
-  for (let i = 0; i <= n / 2; i++) {
-    if (guess === targetNumber) {
-      return guess;
-    } else if (guess < targetNumber) {
-      guess = guess - difference;
-      difference = difference / 2;
-    } else if (guess > targetNumber) {
-      guess = guess + difference;
-      difference = difference / 2;
+function binarySearch(secretNumber, max) {
+  let mid,
+    low = 1,
+    high = max;
+  while (low <= high) {
+    mid = Math.floor((low + high) / 2);
+    if (mid === secretNumber) {
+      return "The random number is " + mid + "!";
+    } else if (mid < secretNumber) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
     }
   }
-  return "error";
+  return "Your target value cannot be found";
 }
 
-console.log(binarySearch(1, 1000));
+let targetNumber = randomNumber(1000);
 
-//try recursion call the binary search function with a new minimum and maximum.
+function timeValues() {
+  let timeArray = [1000, 5000, 10000, 50000, 100000, 500000, 1000000];
+  const binaryMap = timeArray.map(time => checkBinaryTime(time));
+  const linearMap = timeArray.map(time => checkLinearTime(time));
+}
+
+console.log(linearSearch(targetNumber, 999));
+console.log(binarySearch(randomNumber(1000) + 1, 1000));
+/*console.log(checkBinaryTime(1000));
+console.log(checkBinaryTime(50000));
+console.log(checkBinaryTime(10000));
+console.log(checkLinearTime(1000));
+console.log(checkLinearTime(50000));
+console.log(checkLinearTime(10000)); */
+
+console.log(checkLinearTime(timeValues));
+console.log(checkBinaryTime(timeValues));
